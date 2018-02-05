98114f126d3e89cafdd75c586942200ae56ffffb
cww0128 <1113249273@qq.com>
2018-02-05 21:57:28 +0800

    ...

---
+++ application/extra/dolphin.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | 房屋中介系统 [ DolphinPHP ]
+// +----------------------------------------------------------------------
+// | 版权所有 2016~2017 XXX限公司 [ http://www.zrthink.com ]
+// +----------------------------------------------------------------------
+// | 官方网站: http://dolphinphp.com
+// +----------------------------------------------------------------------
+// | 开源协议 ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+
+return [
+    // 产品信息
+    'product_name'      => 'DolphinPHP',
+    'product_version'   => '1.1.0',
+    'build_version'     => '201712171143',
+    'product_website'   => 'http://www.dolphinphp.com',
+    'product_update'    => 'http://www.dolphinphp.com/checkUpdate',
+    'develop_team'      => 'DolphinPHP',
+
+    // 公司信息
+    'company_name'    => 'XXX限公司',
+    'company_website' => 'http://www.zrthink.com',
+];
